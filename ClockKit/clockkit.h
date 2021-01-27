1a561aa1f37234eccfaa8d5845ac226037550d58
Infrasonics <Infrasonics@users.noreply.github.com>
2021-01-27 22:24:19 +0100

    Add missing forward declaration in header (#7)

--- ClockKit/clockkit.h
+++ ClockKit/clockkit.h
@@ -8,6 +8,7 @@
 #include "Common.h"
 
 extern void ckInitialize();
+extern void ckInitializeFromConfig(const char*);
 extern dex::timestamp_t ckTimeAsValue();
 extern const char* ckTimeAsString();
 extern bool ckInSync();
