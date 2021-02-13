0a205ccab6a617c5dbcf9ee34732c66e751ec86c
Camille Goudeseune <cog@illinois.edu>
2021-02-13 12:21:58 -0600

    refactor

--- ClockKit/Timestamp.cpp
+++ ClockKit/Timestamp.cpp
@@ -4,6 +4,8 @@
 #include <string>
 #include "Common.h"
 
+const char* format = "<time %i %i>";
+
 namespace dex {
 
 std::string Timestamp::timestampToString(timestamp_t t)
@@ -11,39 +13,36 @@ std::string Timestamp::timestampToString(timestamp_t t)
     const int sec = t / 1000000;
     const int usec = t % 1000000;
     char buff[256];
-    sprintf(buff, "<time %i %i>", sec, usec);
+    sprintf(buff, format, sec, usec);
     return std::string(buff);
 }
 
 timestamp_t Timestamp::stringToTimestamp(std::string t)
 {
     int sec, usec;
-    if (sscanf(t.c_str(), "<time %i %i>", &sec, &usec) != 2)
+    if (sscanf(t.c_str(), format, &sec, &usec) != 2)
         return 0;
     return sec * 1000000 + usec;
 }
 
+// C++20 will have std::endian.
+#if __BYTE_ORDER == __BIG_ENDIAN
+#define src(i) (i)
+#else
+#define src(i) (7 - (i))
+#endif
+
 void Timestamp::timestampToBytes(timestamp_t time, char* buffer)
 {
-    const char* t = (char*)&time;
-    if (__BYTE_ORDER == __BIG_ENDIAN) {
-        for (int i = 0; i < 8; ++i) buffer[i] = t[i];
-    }
-    else {
-        for (int i = 0; i < 8; ++i) buffer[i] = t[7 - i];
-    }
+    const char* t = (const char*)&time;
+    for (int i = 0; i < 8; ++i) buffer[i] = t[src(i)];
 }
 
 timestamp_t Timestamp::bytesToTimestamp(const char* buffer)
 {
     timestamp_t time;
     char* t = (char*)&time;
-    if (__BYTE_ORDER == __BIG_ENDIAN) {
-        for (int i = 0; i < 8; i++) t[i] = buffer[i];
-    }
-    else {
-        for (int i = 0; i < 8; i++) t[i] = buffer[7 - i];
-    }
+    for (int i = 0; i < 8; ++i) t[i] = buffer[src(i)];
     return time;
 }
 
