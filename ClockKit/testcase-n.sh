2566f4566b6f40a381583d2b33f8de5889dbc58b
Camille Goudeseune <cog@illinois.edu>
2021-02-28 15:59:20 -0600

    cosmic-ray comment

--- ClockKit/testcase-n.sh
+++ ClockKit/testcase-n.sh
@@ -16,6 +16,18 @@ conf=$(mktemp /tmp/clockkit.XXXXXX)
 srv=$(mktemp /tmp/clockkit.XXXXXX)
 clis=''
 for i in $(seq $n); do
+  # Each X can have 26+26+10 values,
+  # so each cli[i] is drawn from N = 62^6 = 5.6e10 possibilities.
+  # So the chance of collision, per the "birthday paradox,"
+  # is roughly 1 - ((N-1)/N) ^ (n choose 2) = 2.6e-10.
+  #
+  # What's the chance of this test failing due to a bit flip from
+  # a cosmic ray?  Such errors occur about 1.4e-15 per byte per second,
+  # http://en.wikipedia.org/wiki/Cosmic_ray#Effect_on_electronics.
+  # So for 30 clients at 4 MB each for 4 seconds, i.e. 480 MB-s,
+  # the chance is 1 - (1-1.4e15) ^ 480e6 = 6.7e-7.
+  #
+  # So 6 Xs are enough.
   cli[i]=$(mktemp /tmp/clockkit.XXXXXX)
   clis+=${cli[i]}
   clis+=' '
