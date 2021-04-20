ecc6809ebdcf5e3a47b7e2d0d2f6586f127aabce
Camille Goudeseune <cog@illinois.edu>
2021-04-20 15:06:51 -0500

    documented

--- ClockKit/config-remote.example.sh
+++ ClockKit/config-remote.example.sh
@@ -1,7 +1,18 @@
-# Config for test2hosts.sh.
+# An example configuration for test2hosts.sh.
+# Copy this to config-remote.sh and edit it.
+# Then test2hosts.sh can run.
 
+# A host that you can ssh to without a password prompt.
 host=myhost.mydomain.com
-port=4567
+
+# On $host, the port that sshd listens to.  Usually 22.
 sshport=123
+
+# On $host, the dir containing ckserver.
 dirRemote=/foo/bar/clockkit/ClockKit
+
+# On $host, the port on which ckserver will listen.
+port=4567
+
+# The timeout for localhost's ./ckphaselock connecting to $host.
 timeout_msec=500
