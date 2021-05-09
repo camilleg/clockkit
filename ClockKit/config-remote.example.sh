8429b73204e4c2146eba1639b8cb1d5c899c3168
camilleg <camilleg@camille-g.com>
2021-05-08 21:45:36 -0500

    howto for firewall holes and ssh keys

--- ClockKit/config-remote.example.sh
+++ ClockKit/config-remote.example.sh
@@ -2,17 +2,26 @@
 # Copy this to config-remote.sh and edit it.
 # Then test2hosts.sh can run.
 
-# A host that you can ssh to with credentials,
-# instead of with a password prompt.
+# A host that you can ssh to with credentials, without a password prompt.
+# To make such credentials:
+#   On localhost, ssh-keygen -t rsa; then append ~/.ssh/id_rsa.pub to $host's ~/.ssh/authorized_keys.
+#   On $host, chmod go-rw ~/.ssh/authorized_keys
+#   On $host in /etc/ssh/sshd_config, set PubkeyAuthentication yes; service ssh --full-restart
 host=myhost.mydomain.com
 
-# On $host, the port that sshd listens to.  Usually 22.
+# On $host, the port that sshd listens to.
+# To change it from the default of 22, to /etc/ssh/sshd_config add a line like `Port 1234`.
+# Then `sudo service ssh --full-restart`.
+# You may need to poke holes in firewalls for this TCP port, e.g.,
+# ufw allow 123/tcp comment 'ssh on nonstandard port'
 sshport=123
 
 # On $host, the dir containing ckserver.
 dirRemote=/foo/bar/clockkit/ClockKit
 
 # On $host, the port on which ckserver will listen.
+# You may need to poke holes in firewalls for this UDP port, e.g.,
+# ufw allow 4444/udp comment ClockKit
 port=4567
 
 # The timeout for localhost's ./ckphaselock connecting to $host.
