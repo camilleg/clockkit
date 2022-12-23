ce7a9bf0ff2230d3111320435da610f4c9ab5368
camilleg <camilleg@camille-g.com>
2022-12-23 11:40:49 -0600

    argv handling

--- ClockKit/ClockServerMain.cpp
+++ ClockKit/ClockServerMain.cpp
@@ -8,20 +8,13 @@
 
 int main(int argc, char* argv[])
 {
-    auto addr = std::string("127.0.0.1");
-    auto port = 4444;
-
     if (argc < 2) {
         std::cerr << "usage: " << argv[0] << " [<ipaddr>] <port>\n";
         return 1;
     }
-    else if (argc == 3) {
-        addr = std::string(argv[1]);
-        port = dex::parseInt(argv[2]);
-    }
-    else {
-        port = dex::parseInt(argv[1]);
-    }
+    const auto haveIP = argc == 3;
+    const std::string addr(haveIP ? argv[1] : "127.0.0.1");
+    const auto port = dex::parseInt(argv[haveIP ? 2 : 1]);
 
 #ifdef DEBUG
     // A slow clock, for testing.
